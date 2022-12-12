8a9b0ccbe9994f74d9ea7a2d101bf1cb14012299
Alan Wan <alan.wan@oocl.com>
2022-12-12 15:46:21 +0800

    fear: add static ui for counter

---
+++ src/features/Counter.js
@@ -0,0 +1,14 @@
+// react functional component
+// rfc
+
+import React from "react";
+
+export default function Counter() {
+    return (
+        <div>Counter component
+            <button>+</button>
+            <span>0</span>
+            <button>-</button>
+        </div>
+    )
+}
