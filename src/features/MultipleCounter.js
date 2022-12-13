63d234fcdcf1a75dc50874442935652ebb829c10
Alan Wan <alan.wan@oocl.com>
2022-12-13 10:34:37 +0800

    fear: change sum value from parent

--- src/features/MultipleCounter.js
+++ src/features/MultipleCounter.js
@@ -1,18 +1,25 @@
 import React, {useState} from 'react'
 import CounterGroup from './CounterGroup'
 import CounterSizeGenerator from './CounterSizeGenerator'
+import CounterGroupSum from './CounterGroupSum';
 
 export default function MultipleCounter() {
     const [size, setSize] = useState(0);
+    const [sum, setSum] = useState(0);
 
     const updateSize = (sizeNumber) => {
         console.log(sizeNumber);
         setSize(sizeNumber);
     }
+
+    const updateSum = (change) => {
+      setSize(sum +change);
+    }
   return (
     <div>
         <CounterSizeGenerator updateSize2 = {updateSize}/>
-        <CounterGroup size={size}/>
+        <CounterGroupSum />
+        <CounterGroup size={size} updateSum={updateSum}/>
     </div>
   )
 }
